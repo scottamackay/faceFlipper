a0329a8f278fd351819c5510ae4d135af7b081fe
john.lai@topdecileinc.com <john.lai@topdecileinc.com>
2016-07-17 01:00:17 +0000

    made a sample db file

---
+++ db.sample.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  crypto    = require('crypto'),
+  _   = require('lodash'),
+  db = {};
+
+/*
+ * For injection attacks
+ */
+var escapeProperty = function(value) {
+  return _.escape(value);
+};
+
+/*
+ * Email validation
+ */
+var validateUniqueEmail = function(value, callback) {
+  var User = mongoose.model('User');
+  User.find({
+    $and: [{
+      email: value
+    }, {
+      _id: {
+        $ne: this._id
+      }
+    }]
+  }, function(err, user) {
+    callback(err || user.length === 0);
+  });
+};
+
+// User model
+var UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+    get: escapeProperty
+  },
+  lastname: {
+    type: String,
+    required: true,
+    get: escapeProperty
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
+    validate: [validateUniqueEmail, 'E-mail address is already in-use']
+  },
+  winnerIndex: {
+    type: Number,
+    default: 0
+  },
+  postal: String,
+  date: Date,
+  createdAt: Date,
+  image: {
+    name: String,
+    sliceTop: String,
+    sliceMiddle: String,
+    sliceBottom: String
+  }
+});
+
+mongoose.model('User', UserSchema);
+mongoose.connect('localhost', 'faceflipper', function(err) {
+  if (err) {
+    console.log('Could not connect to database: ' + err);
+    process.exit(1);
+  }
+});
